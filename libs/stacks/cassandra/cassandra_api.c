4390b50a62d06b7579aa6dd191bfc48f7198d3d8
kapeawpc <kapeawpc@gmail.com>
2016-10-31 15:42:57 +0700

    //time_t     now;

--- libs/stacks/cassandra/cassandra_api.c
+++ libs/stacks/cassandra/cassandra_api.c
@@ -2,11 +2,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include "cassandra_api.h"
+#include <time.h>       /* time_t, time (for timestamp in second) */
 
 
 CassCluster* cass_api_create_cluster(const char* hosts, const char *user, const char *passwd, int n_worker, int queues, int init_connection, int max_connection){
   CassCluster* cluster = cass_cluster_new();
-    cass_cluster_set_protocol_version(cluster,2);
+    //cass_cluster_set_protocol_version(cluster,2);
     cass_cluster_set_contact_points(cluster, hosts);
     cass_cluster_set_credentials(cluster, user, passwd);
     cass_cluster_set_num_threads_io(cluster, n_worker);
@@ -67,8 +68,8 @@ int cass_api_get_result_json(const CassResult *result, CASS_API_BUFFER *buffer, 
 int cass_api_get_result_column(const CassResult *result, CASS_API_BUFFER *buffer, char *error, const CassSession* session){
 
     const CassSchemaMeta* schema_meta = cass_session_get_schema_meta(session);
-    const CassKeyspaceMeta* keyspace_meta = cass_schema_meta_keyspace_by_name(schema_meta, "examples");
-    const CassTableMeta* table_meta = cass_keyspace_meta_table_by_name(keyspace_meta, "peaw");
+    const CassKeyspaceMeta* keyspace_meta = cass_schema_meta_keyspace_by_name(schema_meta, "peaw");
+    const CassTableMeta* table_meta = cass_keyspace_meta_table_by_name(keyspace_meta, "test_table");
 
     
     int indent=0;
@@ -87,24 +88,6 @@ int cass_api_get_result_column(const CassResult *result, CASS_API_BUFFER *buffer
       if(row){
           i += sprintf(&buffer->data[i], "{");
 
-
-
-       /*Get_Table*/ 
-       /*CassIterator* iterator_table = cass_iterator_tables_from_keyspace_meta(keyspace_meta);
-        while (cass_iterator_next(iterator_table)) {
-              const CassTableMeta* meta_table = cass_iterator_get_table_meta(iterator_table);
-                    const char* name;
-                    size_t name_length;
-                    cass_table_meta_name(meta_table, &name, &name_length);  
-                    
-                  if (strcmp(name, "peaw")==0) {
-                              
-                    i += sprintf(&buffer->data[i], "\"Table\":\"%.*s\",\n", (int)name_length,name);
-
-                    }
-                  }
-              */
-
           int col = 0;
           //for(;;col++){
 
@@ -112,144 +95,150 @@ int cass_api_get_result_column(const CassResult *result, CASS_API_BUFFER *buffer
                 while (cass_iterator_next(iterator_colunm)) {
                   const CassValue* value = cass_row_get_column(row, col);
                   if( value == NULL){break;}
-                  //const CassValue* meta = cass_row_get_column_by_name(row, "uid");
-
-                  const char* col_value = NULL;
-                  size_t length;
-                  cass_value_get_string(value, &col_value, &length);
-
-
-
-                  const CassColumnMeta* meta = cass_iterator_get_column_meta(iterator_colunm);
-                  const char* name;
-                  size_t name_length;
-                  cass_column_meta_name(meta, &name, &name_length);
-
-                  if( col_value != NULL){
-                      i += sprintf(&buffer->data[i], "\"%.*s\":", (int)name_length,name);
-                      i += sprintf(&buffer->data[i], "\"%.*s\",\n", (int)length,col_value);
-                      col = col +1;
-                      //continue;
+                
+                      //get_name_column
+                      const CassColumnMeta* meta = cass_iterator_get_column_meta(iterator_colunm);
+                      const char* name;
+                      size_t name_length;
+                      cass_column_meta_name(meta, &name, &name_length);
 
 
-                  }else if( col_value == NULL){
-                      i += sprintf(&buffer->data[i], "\"%.*s\":", (int)name_length,name);
-                      i += sprintf(&buffer->data[i], "\"%.*s\",\n", (int)length,col_value);
-                      col = col +1;
-                    //continue;
-                   } 
+                      const char* col_value = NULL;
+                      size_t length;
 
-                //while
-           }
-         
+                      cass_int64_t t;
+                      cass_int32_t it;
+                      cass_bool_t b;
+                      cass_double_t d;
+                      CassUuid u;
+                      char us[CASS_UUID_STRING_LENGTH];
+
+                      //time
+                      struct tm  timeinfo;
+                      char       buf[80];
+
+
+                      if( col_value != NULL || col_value == NULL){col = col +1;}
+
+                    
+                      CassValueType type = cass_value_type(value);
+                      switch (type) {
+                        case CASS_VALUE_TYPE_INT:
+                          if (cass_value_is_null(value)) {
+                            i += sprintf(&buffer->data[i], "\"%.*s\":", (int)name_length,name);
+                            i += sprintf(&buffer->data[i], "\"NULL\",\n");
+                          break;
+                          }else{
+                            cass_value_get_int32(value, &it);
+                            i += sprintf(&buffer->data[i], "\"%.*s\":", (int)name_length,name);
+                            i += sprintf(&buffer->data[i], "\"%d\",\n",it);
+                          break;
+                          }
+
+                        case CASS_VALUE_TYPE_BOOLEAN:
+                        
+                          //printf("%s", b ? "true" : "false");
+                          if (cass_value_is_null(value)) {
+                            i += sprintf(&buffer->data[i], "\"%.*s\":", (int)name_length,name);
+                            i += sprintf(&buffer->data[i], "\"NULL\",\n");
+                          break;
+                          }else{
+                            cass_value_get_bool(value, &b);
+                            i += sprintf(&buffer->data[i], "\"%.*s\":", (int)name_length,name);
+                            i += sprintf(&buffer->data[i], "%s", b ? "true" : "false");
+                          break;
+                          }
+
+                        case CASS_VALUE_TYPE_DOUBLE:
+                         // printf("%f", d);
+                         if (cass_value_is_null(value)) {
+                            i += sprintf(&buffer->data[i], "\"%.*s\":", (int)name_length,name);
+                            i += sprintf(&buffer->data[i], "\"NULL\",\n");
+                          break;
+                          }else{
+                           cass_value_get_double(value, &d);
+                            i += sprintf(&buffer->data[i], "\"%.*s\":", (int)name_length,name);
+                            i += sprintf(&buffer->data[i], "\"%f\",\n",d);
+                          break;
+                          }
+
+                        case CASS_VALUE_TYPE_TEXT:
+                        case CASS_VALUE_TYPE_ASCII:
+                        case CASS_VALUE_TYPE_VARCHAR:
+                        if (cass_value_is_null(value)) {
+                          i += sprintf(&buffer->data[i], "\"%.*s\":", (int)name_length,name);
+                          i += sprintf(&buffer->data[i], "\"NULL\",\n");
+                          break;
+                         }else{
+
+                            cass_value_get_string(value, &col_value, &length);
+                            i += sprintf(&buffer->data[i], "\"%.*s\":", (int)name_length,name);
+                            i += sprintf(&buffer->data[i], "\"%.*s\",\n", (int)length,col_value);
+                          //printf("\"%.*s\"\n", (int)length, col_value);
+                          break;
+                        }
+
+                        case CASS_VALUE_TYPE_UUID:
+                         
+                          if (cass_value_is_null(value)) {
+                            i += sprintf(&buffer->data[i], "\"%.*s\":", (int)name_length,name);
+                            i += sprintf(&buffer->data[i], "\"NULL\",\n");
+                          break;
+                         }else{
+
+                          cass_value_get_uuid(value, &u);
+                          cass_uuid_string(u, us);
+                          //printf("%s", us);
+                            i += sprintf(&buffer->data[i], "\"%.*s\":", (int)name_length,name);
+                            i += sprintf(&buffer->data[i], "\"%s\",\n", us);
+                         
+                          break;
+                        }
+
+                       case CASS_VALUE_TYPE_TIMESTAMP:
+
+                         if (cass_value_is_null(value)) {
+                              i += sprintf(&buffer->data[i], "\"%.*s\":", (int)name_length,name);
+                              i += sprintf(&buffer->data[i], "\"NULL\",\n");
+                            break;
+                        }else{
+
+                            
+                            cass_value_get_int64(value, &t);
+                           
+                            time_t tt = (time_t)t/1000;
+
+                            // Format time, "ddd yyyy-mm-dd hh:mm:ss zzz"
+                             timeinfo = *localtime(&tt);
+                             strftime(buf, sizeof(buf), " %Y-%m-%d %H:%M:%S %z", &timeinfo);
+
+                            i += sprintf(&buffer->data[i], "\"%.*s\":", (int)name_length,name);
+                            i += sprintf(&buffer->data[i], "\"%s\",\n", buf);
+
+                          break;
+                        }
+
+                        default:
+                          
+                            if (cass_value_is_null(value)) {
+                              i += sprintf(&buffer->data[i], "\"%.*s\":", (int)name_length,name);
+                              i += sprintf(&buffer->data[i], "\"NULL\",\n");
+                            } else {
+                              i += sprintf(&buffer->data[i], "\"%.*s\":", (int)name_length,name);
+                              i += sprintf(&buffer->data[i], "\"<unhandled type>\",\n");
+
+                            }
+
+                          break;
+                      }
+                  }  
    
-        i += sprintf(&buffer->data[i], "},\n");
-      }
+                i += sprintf(&buffer->data[i], "},\n");
+              }
+            }
+
+        i += sprintf(&buffer->data[i], "}");
+        printf("%s\n", buffer->data);
+        return CASS_API_OK;
     }
 
-    i += sprintf(&buffer->data[i], "}");
-    printf("%s\n", buffer->data);
-    return CASS_API_OK;
-}
-
-
-
-
-/*void print_keyspace_meta(const CassKeyspaceMeta* meta, int indent);
-void print_table_meta(const CassTableMeta* meta, int indent);
-void print_column_meta(const CassColumnMeta* meta, int indent);
-
-
-void print_keyspace(CassSession* session, const char* keyspace) {
-  const CassSchemaMeta* schema_meta = cass_session_get_schema_meta(session);
-  const CassKeyspaceMeta* keyspace_meta = cass_schema_meta_keyspace_by_name(schema_meta, keyspace);
-
-  if (keyspace_meta != NULL) {
-    print_keyspace_meta(keyspace_meta, 0);
-  } else {
-    fprintf(stderr, "Unable to find \"%s\" keyspace in the schema metadata\n", keyspace);
-  }
-
-  cass_schema_meta_free(schema_meta);
-}
-
-
-void print_table(CassSession* session, const char* keyspace, const char* table) {
-  const CassSchemaMeta* schema_meta = cass_session_get_schema_meta(session);
-  const CassKeyspaceMeta* keyspace_meta = cass_schema_meta_keyspace_by_name(schema_meta, keyspace);
-
-  if (keyspace_meta != NULL) {
-    const CassTableMeta* table_meta = cass_keyspace_meta_table_by_name(keyspace_meta, table);
-    if (table_meta != NULL) {
-      print_table_meta(table_meta, 0);
-    } else {
-      fprintf(stderr, "Unable to find \"%s\" table in the schema metadata\n", table);
-    }
-  } else {
-    fprintf(stderr, "Unable to find \"%s\" keyspace in the schema metadata\n", keyspace);
-  }
-
-  cass_schema_meta_free(schema_meta);
-}
-
-
-void print_indent(int indent) {
-  int i;
-  for (i = 0; i < indent; ++i) {
-    printf("\t");
-  }
-}
-
-
-void print_keyspace_meta(const CassKeyspaceMeta* meta, int indent) {
-  const char* name;
-  size_t name_length;
-  CassIterator* iterator;
-
-  print_indent(indent);
-  cass_keyspace_meta_name(meta, &name, &name_length);
-  printf("Keyspace \"%.*s\":\n", (int)name_length, name);
-
-  //print_meta_fields(cass_iterator_fields_from_keyspace_meta(meta), indent + 1);
-  printf("\n");
-
-  iterator = cass_iterator_tables_from_keyspace_meta(meta);
-  while (cass_iterator_next(iterator)) {
-    print_table_meta(cass_iterator_get_table_meta(iterator), indent + 1);
-  }
-  printf("\n");
-
-  cass_iterator_free(iterator);
-}
-
-void print_table_meta(const CassTableMeta* meta, int indent) {
-  const char* name;
-  size_t name_length;
-  CassIterator* iterator;
-
-  print_indent(indent);
-  cass_table_meta_name(meta, &name, &name_length);
-  printf("Table \"%.*s\":\n", (int)name_length, name);
-
-  //print_meta_fields(cass_iterator_fields_from_table_meta(meta), indent + 1);
-  printf("\n");
-
-  iterator = cass_iterator_columns_from_table_meta(meta);
-  while (cass_iterator_next(iterator)) {
-   print_column_meta(cass_iterator_get_column_meta(iterator), indent + 1);
-  }
-  printf("\n");
-
-  cass_iterator_free(iterator);
-}
-
-  void print_column_meta(const CassColumnMeta* meta, int indent) {
-  const char* name;
-  size_t name_length;
-  print_indent(indent);
-  cass_column_meta_name(meta, &name, &name_length);
-  printf("Column \"%.*s\":\n", (int)name_length, name);
-  //print_meta_fields(cass_iterator_fields_from_column_meta(meta), indent + 1);
-
-  printf("\n");
-} */
