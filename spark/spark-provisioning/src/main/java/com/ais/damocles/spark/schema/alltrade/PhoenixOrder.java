7b8eea6d30616a6d9314eb1a326c4952e106485a
Peaw Cokezaza <kapeawpc@gmail.com>
2016-12-19 10:34:39 +0700

    update_phoenix

--- spark/spark-provisioning/src/main/java/com/ais/damocles/spark/schema/alltrade/PhoenixOrder.java
+++ spark/spark-provisioning/src/main/java/com/ais/damocles/spark/schema/alltrade/PhoenixOrder.java
@@ -63,6 +63,68 @@ public class PhoenixOrder extends com.ais.damocles.spark.schema.CommonSchema {
 
 	@Column(name = "SaNo")
 	private String SaNo;
+	@Column(name = "rowId")
+	private String rowId;
+
+	@Column(name = "created")
+	private String created;
+
+	@Column(name = "createdBy")
+	private String createdBy;
+
+	@Column(name = "lastUpd")
+	private String lastUpd;
+
+	@Column(name = "lastUpdBy")
+	private String lastUpdBy;
+
+	@Column(name = "accntId")
+	private String accntId;
+
+	@Column(name = "orderDt")
+	private String orderDt;
+
+	@Column(name = "orderChannel")
+	private String orderChannel;
+
+	@Column(name = "submitedDt")
+	private String submitedDt;
+
+	@Column(name = "completedDt")
+	private String completedDt;
+
+	@Column(name = "locationCd")
+	private String locationCd;
+
+	@Column(name = "statusCd")
+	private String statusCd;
+
+	@Column(name = "statusDt")
+	private String statusDt;
+
+	@Column(name = "priorityCd")
+	private String priorityCd;
+
+	@Column(name = "modificationNum")
+	private String modificationNum;
+
+	@Column(name = "sffOrderFees")
+	private String sffOrderFees;
+
+	@Column(name = "sffOrderDocumentTrackings")
+	private String sffOrderDocumentTrackings;
+
+	@Column(name = "sffOrderServiceInstances")
+	private String sffOrderServiceInstances;
+
+	@Column(name = "sffOrderAccountPromotions")
+	private String sffOrderAccountPromotions;
+
+	@Column(name = "locationId")
+	private String locationId;
+
+	@Column(name = "smartCardFlg")
+	private String smartCardFlg;
 
 	public PhoenixOrder() {
 
@@ -73,7 +135,15 @@ public class PhoenixOrder extends com.ais.damocles.spark.schema.CommonSchema {
 			String ErrorDestination, String LocationCode, String BaCategory,
 			String BaSegment, String BillingSystem, String PromotionCode,
 			String ServiceCode, String WoItemErrorMsg, String DcpDestination,
-			String DcpDestinationStatus, String BaNo, String CaNo, String SaNo) {
+			String DcpDestinationStatus, String BaNo, String CaNo, String SaNo,
+			String rowId, String created, String createdBy, String lastUpd,
+			String lastUpdBy, String accntId, String orderDt,
+			String orderChannel, String submitedDt, String completedDt,
+			String locationCd, String statusCd, String statusDt,
+			String priorityCd, String modificationNum, String sffOrderFees,
+			String sffOrderDocumentTrackings, String sffOrderServiceInstances,
+			String sffOrderAccountPromotions, String locationId,
+			String smartCardFlg) {
 
 		this.orderNo = orderNo;
 		this.columnMapper.put("orderNo", "orderNo");
@@ -132,6 +202,71 @@ public class PhoenixOrder extends com.ais.damocles.spark.schema.CommonSchema {
 		this.SaNo = SaNo;
 		this.columnMapper.put("SaNo", "SaNo");
 
+		this.rowId = rowId;
+		this.columnMapper.put("rowId", "rowId");
+
+		this.created = created;
+		this.columnMapper.put("created", "created");
+
+		this.createdBy = createdBy;
+		this.columnMapper.put("createdBy", "createdBy");
+
+		this.lastUpd = lastUpd;
+		this.columnMapper.put("lastUpd", "lastUpd");
+
+		this.lastUpdBy = lastUpdBy;
+		this.columnMapper.put("lastUpdBy", "lastUpdBy");
+
+		this.accntId = accntId;
+		this.columnMapper.put("accntId", "accntId");
+
+		this.orderDt = orderDt;
+		this.columnMapper.put("orderDt", "orderDt");
+
+		this.orderChannel = orderChannel;
+		this.columnMapper.put("orderChannel", "orderChannel");
+
+		this.submitedDt = submitedDt;
+		this.columnMapper.put("submitedDt", "submitedDt");
+
+		this.completedDt = completedDt;
+		this.columnMapper.put("completedDt", "completedDt");
+
+		this.locationCd = locationCd;
+		this.columnMapper.put("locationCd", "locationCd");
+
+		this.statusCd = statusCd;
+		this.columnMapper.put("statusCd", "statusCd");
+
+		this.statusDt = statusDt;
+		this.columnMapper.put("statusDt", "statusDt");
+
+		this.priorityCd = priorityCd;
+		this.columnMapper.put("priorityCd", "priorityCd");
+
+		this.modificationNum = modificationNum;
+		this.columnMapper.put("modificationNum", "modificationNum");
+
+		this.sffOrderFees = sffOrderFees;
+		this.columnMapper.put("sffOrderFees", "sffOrderFees");
+
+		this.sffOrderDocumentTrackings = sffOrderDocumentTrackings;
+		this.columnMapper.put("sffOrderDocumentTrackings",
+				"sffOrderDocumentTrackings");
+
+		this.sffOrderServiceInstances = sffOrderServiceInstances;
+		this.columnMapper.put("sffOrderServiceInstances",
+				"sffOrderServiceInstances");
+
+		this.sffOrderAccountPromotions = sffOrderAccountPromotions;
+		this.columnMapper.put("sffOrderAccountPromotions",
+				"sffOrderAccountPromotions");
+
+		this.locationId = locationId;
+		this.columnMapper.put("locationId", "locationId");
+
+		this.smartCardFlg = smartCardFlg;
+		this.columnMapper.put("smartCardFlg", "smartCardFlg");
 	}
 
 	public String getOrderNo() {
@@ -288,6 +423,166 @@ public class PhoenixOrder extends com.ais.damocles.spark.schema.CommonSchema {
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
+	}
+
+	public String getRowId() {
+		return rowId;
+	}
+
+	public void setRowId(String rowId) {
+		this.rowId = rowId;
+	}
+
+	public String getCreated() {
+		return created;
+	}
+
+	public void setCreated(String created) {
+		this.created = created;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getLastUpd() {
+		return lastUpd;
+	}
+
+	public void setLastUpd(String lastUpd) {
+		this.lastUpd = lastUpd;
+	}
+
+	public String getLastUpdBy() {
+		return lastUpdBy;
+	}
+
+	public void setLastUpdBy(String lastUpdBy) {
+		this.lastUpdBy = lastUpdBy;
+	}
+
+	public String getAccntId() {
+		return accntId;
+	}
+
+	public void setAccntId(String accntId) {
+		this.accntId = accntId;
+	}
+
+	public String getOrderDt() {
+		return orderDt;
+	}
+
+	public void setOrderDt(String orderDt) {
+		this.orderDt = orderDt;
+	}
+
+	public String getOrderChannel() {
+		return orderChannel;
+	}
+
+	public void setOrderChannel(String orderChannel) {
+		this.orderChannel = orderChannel;
+	}
+
+	public String getSubmitedDt() {
+		return submitedDt;
+	}
+
+	public void setSubmitedDt(String submitedDt) {
+		this.submitedDt = submitedDt;
+	}
+
+	public String getCompletedDt() {
+		return completedDt;
+	}
+
+	public void setCompletedDt(String completedDt) {
+		this.completedDt = completedDt;
+	}
+
+	public String getLocationCd() {
+		return locationCd;
+	}
+
+	public void setLocationCd(String locationCd) {
+		this.locationCd = locationCd;
+	}
+
+	public String getStatusDt() {
+		return statusDt;
+	}
+
+	public void setStatusDt(String statusDt) {
+		this.statusDt = statusDt;
+	}
+
+	public String getPriorityCd() {
+		return priorityCd;
+	}
+
+	public void setPriorityCd(String priorityCd) {
+		this.priorityCd = priorityCd;
+	}
+
+	public String getModificationNum() {
+		return modificationNum;
+	}
+
+	public void setModificationNum(String modificationNum) {
+		this.modificationNum = modificationNum;
+	}
+
+	public String getSffOrderFees() {
+		return sffOrderFees;
+	}
+
+	public void setSffOrderFees(String sffOrderFees) {
+		this.sffOrderFees = sffOrderFees;
+	}
+
+	public String getSffOrderDocumentTrackings() {
+		return sffOrderDocumentTrackings;
+	}
+
+	public void setSffOrderDocumentTrackings(String sffOrderDocumentTrackings) {
+		this.sffOrderDocumentTrackings = sffOrderDocumentTrackings;
+	}
+
+	public String getSffOrderServiceInstances() {
+		return sffOrderServiceInstances;
+	}
+
+	public void setSffOrderServiceInstances(String sffOrderServiceInstances) {
+		this.sffOrderServiceInstances = sffOrderServiceInstances;
+	}
+
+	public String getSffOrderAccountPromotions() {
+		return sffOrderAccountPromotions;
+	}
+
+	public void setSffOrderAccountPromotions(String sffOrderAccountPromotions) {
+		this.sffOrderAccountPromotions = sffOrderAccountPromotions;
+	}
+
+	public String getLocationId() {
+		return locationId;
+	}
+
+	public void setLocationId(String locationId) {
+		this.locationId = locationId;
+	}
+
+	public String getSmartCardFlg() {
+		return smartCardFlg;
+	}
+
+	public void setSmartCardFlg(String smartCardFlg) {
+		this.smartCardFlg = smartCardFlg;
 	}
 
 }
